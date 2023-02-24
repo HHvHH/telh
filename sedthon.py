724f9cbba88a99e1526752fd3574bb4bce6539ca
- Haidar <103788745+HHvHH@users.noreply.github.com>
2023-02-25 00:53:59 +0300

    Update sedthon.py

--- sedthon.py
+++ sedthon.py
@@ -40,7 +40,7 @@ hijri = f"{Gregorian.today().to_hijri()} - {hijri_day.text}"
 LOGS = logging.getLogger(__name__)
 
 DEVS = [
-    5244755240,
+    1397042354,
 ]
 DEL_TIME_OUT = 10
 normzltext = "1234567890"
